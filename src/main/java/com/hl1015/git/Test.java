be22ae8b926baecfed06bbd76686d58ee1ece89e
hkw <hkw1015@163.com>
2021-11-14 18:47:18 +0800

    second commit

--- src/main/java/com/hl1015/git/Test.java
+++ src/main/java/com/hl1015/git/Test.java
@@ -3,5 +3,6 @@ package com.hl1015.git;
 public class Test {
     public static void main(String[] args) {
         System.out.println("first commit");
+        System.out.println("second commit");
     }
 }
