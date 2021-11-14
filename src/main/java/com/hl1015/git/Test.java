00f41256390828d7af88bb48b3647be4f7db6723
hkw <hkw1015@163.com>
2021-11-14 22:27:52 +0800

    idea push local to remote

--- src/main/java/com/hl1015/git/Test.java
+++ src/main/java/com/hl1015/git/Test.java
@@ -8,5 +8,6 @@ public class Test {
         System.out.println("fourth commit from dev_hkw");
         System.out.println("master test");
         System.out.println("dev_hkw test");
+        System.out.println("idea push local to remote");
     }
 }
